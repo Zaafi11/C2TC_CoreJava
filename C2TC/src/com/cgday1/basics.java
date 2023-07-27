2369ea277432884a2f8effb36a56e24e59e9b6bd
zaafira11 <zaafiranpm@gmail.com>
2023-07-27 12:47:32 +0530

    First commit

---
+++ C2TC/src/com/cgday1/basics.java
@@ -0,0 +1,19 @@
+package com.cgday1;
+
+public class basics {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=45;
+		System.out.println("Add: " +(a+b));
+		System.out.println("Sub: " +(a-b));
+		System.out.println("Mul: " +(a*b));
+		System.out.println("Div: " +(a/b));
+		System.out.println("Mod: " +(a%b));
+		
+		
+
+	}
+
+}
