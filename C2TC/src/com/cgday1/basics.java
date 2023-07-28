47c25abd7e7441887564394cbeb1166ecb349d4b
zaafira11 <zaafiranpm@gmail.com>
2023-07-28 11:19:37 +0530

    second commit

--- C2TC/src/com/cgday1/basics.java
+++ C2TC/src/com/cgday1/basics.java
@@ -4,6 +4,7 @@ public class basics {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//arthimetic
 		int a=10;
 		int b=45;
 		System.out.println("Add: " +(a+b));
@@ -11,7 +12,26 @@ public class basics {
 		System.out.println("Mul: " +(a*b));
 		System.out.println("Div: " +(a/b));
 		System.out.println("Mod: " +(a%b));
-		
+        
+        //boolean
+        boolean x = true;
+        boolean y = false;
+
+        boolean andResult = x && y;
+        boolean orResult = x || y;
+        boolean notX = !x;
+        boolean notY = !y;
+
+        System.out.println("x AND y: " + andResult);
+        System.out.println("x OR y: " + orResult);
+        System.out.println("NOT x: " + notX);
+        System.out.println("NOT y: " + notY);
+        //increment & decrement
+        a++; // Increment 
+        b--; //Decrement
+        System.out.println(a);
+        System.out.println(b);
+
 		
 
 	}
---
+++ C2TC/src/com/cgday2/singleinherit.java
@@ -0,0 +1,11 @@
+package com.cgday2;
+class single{
+	public void singlepen() {
+		System.out.println("single");
+}
+}
+
+public class singleinherit {
+	
+
+}
